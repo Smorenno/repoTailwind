2671a565f8a0d616ce2f01d97400a2be60eec18a
Smorenno <ser.mpc00@gmail.com>
2025-03-06 12:49:14 +0100

    Cambios antes de usar story book

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // Habilita el modo oscuro con la clase 'dark'
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
